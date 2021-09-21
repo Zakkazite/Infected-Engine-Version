15dfe8ace66f9dc7840b77c1726fa590cdfd7497
CrazyCode <86667557+Zakkazite@users.noreply.github.com>
2021-09-21 22:56:11 +0600

    Version.lua

---
+++ Version.lua
@@ -0,0 +1,7 @@
+local key = game.Players.LocalPlayer.Backpack:FindFirstChildWhichIsA("Tool")
+
+local function texP()
+	script.Parent.Text = "Player Has "..key.Name.." | ".."Infected Engine 1.3 Revamp"
+end
+
+texP()
